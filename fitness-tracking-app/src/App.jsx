8f965f1dbc71672ee5224f47150e7b1dc7770185
ipz06 <it.vania.9@gmail.com>
2023-05-19 15:09:30 +0300

    Add initial commit

---
+++ fitness-tracking-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { ChakraProvider } from "@chakra-ui/react";
+
+
+function App() {
+
+  return (
+    <ChakraProvider>
+      <div>
+        <p>hello</p>   
+      </div>
+    </ChakraProvider>
+  )
+}
+
+export default App
