a501e85c6db74c6a97c298ff18673618c503b6f2
Ivan Damyankin <idamyankin@gmail.com>
2023-06-09 18:08:32 +0300

    Fix responsive designe in friend requests

--- fitness-tracking-app/src/components/FriendRequests/FriendRequests.jsx
+++ fitness-tracking-app/src/components/FriendRequests/FriendRequests.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, useContext } from 'react';
-import { Box, Image, Text, VStack, Button, Flex, SimpleGrid } from '@chakra-ui/react';
+import { Box, Image, Text, VStack, HStack, Button, Flex, SimpleGrid } from '@chakra-ui/react';
 import { getUserFriendRequests } from '../../services/friends.service';
 import { AuthContext } from '../../common/context';
 import { saveFriendToDatabase } from '../../services/friends.service';
@@ -64,46 +64,59 @@ const FriendRequests = () => {
 	}
   };
 //key={request.friendRequestKey}
-  return (
-    <Box p={4} maxW="65%" pt="2%">
-      <SimpleGrid  columns={[1, 2, 3 ]}  gap={4}>
+return (
+  <Box p={4} maxW="65%" pt="2%">
+    <SimpleGrid columns={[1, 2, 3]} gap={4}>
       {friendRequests.map((request) => (
-         
-              <Box
-              key={request.friendRequestKey}
-              p={4}
-              borderWidth="1px"
-              borderRadius="sm"
-              boxShadow="md"
-              display="flex"
-              alignItems="center"
-              minW={{ base: "85%", sm: "60%", md: "40%" }} 
-      maxW="100%"
-            >
-          <Box display="flex" alignItems="center" flexWrap="wrap"  flexDirection={{ base: "column", sm: "row" }}>
-            <Image src={request.photo} alt="Profile" boxSize="22%" borderRadius="full" mr={4} />
-            <VStack align="start">
-              <Text fontStyle="normal" fontWeight="bold">{request.sender}</Text>
-              <Text fontStyle="normal" fontSize={{ base: "xs", sm: "sm", md: "md" }}>{request.email}</Text>
-            </VStack>
-          </Box>
-          <Box display="flex" gap={2} ml={{ base: "2%", md: "10%" }} flexWrap="wrap">
-            <Button fontSize={{ base: "xs", sm: "sm", md: "sm" }} borderRadius="sm" color="blackAlpha.900" backgroundColor="teal.200" maxW={{ base: "80%", sm: "40%", md: "100%" }} size="sm"  onClick={() => handleAcceptRequest(request.photo, request.sender, request.email, request.friendRequestKey)} >
-              Accept
-            </Button>
-            <Button fontSize={{ base: "xs", sm: "sm", md: "sm" }} borderRadius="sm" backgroundColor="red.500" color="blackAlpha.900" maxW={{ base: "80%", sm: "40%", md: "100%" }} size="sm"  _hover={{ bg: "red.300" }} onClick={() => handleDeclineRequest(request.friendRequestKey)} >
-              Decline
-            </Button>
-          </Box>
+        <Box
+          key={request.friendRequestKey}
+          p={4}
+          borderWidth="1px"
+          borderRadius="sm"
+          boxShadow="md"
+        >
+          <VStack spacing={4} align="stretch">
+            <HStack spacing={4}>
+              <Image src={request.photo} alt="Profile" boxSize="22%" borderRadius="full" />
+              <VStack align="start">
+                <Text fontStyle="normal" fontSize={{ base: "xs", sm: "sm", md: "md" }} fontWeight="bold">{request.sender}</Text>
+                <Text fontStyle="normal" fontSize={{ base: "xs", sm: "sm", md: "md" }}>{request.email}</Text>
+              </VStack>
+            </HStack>
+            <HStack justifyContent="space-between">
+              <Button
+                fontSize={{ base: "xs", sm: "sm", md: "sm" }}
+                borderRadius="sm"
+                color="blackAlpha.900"
+                backgroundColor="teal.200"
+                size="sm"
+                w="45%"
+                onClick={() => handleAcceptRequest(request.photo, request.sender, request.email, request.friendRequestKey)}
+              >
+                Accept
+              </Button>
+              <Button
+                fontSize={{ base: "xs", sm: "sm", md: "sm" }}
+                borderRadius="sm"
+                backgroundColor="red.500"
+                color="blackAlpha.900"
+                size="sm"
+                w="45%"
+                _hover={{ bg: "red.300" }}
+                onClick={() => handleDeclineRequest(request.friendRequestKey)}
+              >
+                Decline
+              </Button>
+            </HStack>
+          </VStack>
         </Box>
-       
       ))}
-       </SimpleGrid>
-      {friendRequests.length === 0 && (
-        <Text fontStyle="normal">No friend requests</Text>
-      )}
-    </Box>
-  );
+    </SimpleGrid>
+    {friendRequests.length === 0 && (
+      <Text fontStyle="normal">No friend requests</Text>
+    )}
+  </Box>
+);
 };
 
 export default FriendRequests;
