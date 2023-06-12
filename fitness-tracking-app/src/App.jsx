08b53875063d14c47e4d218c30a4dbb375e89474
ipz06 <it.vania.9@gmail.com>
2023-06-12 10:40:24 +0300

    Add nav link to about and error page

--- fitness-tracking-app/src/App.jsx
+++ fitness-tracking-app/src/App.jsx
@@ -1,4 +1,4 @@
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import Navbar from "./components/Navbar/Navbar";
@@ -17,11 +17,11 @@ import Goals from "./pages/Goals/Goals";
 import AuthenticateRoute from "./hoc/AuthenticateRoute";
 import LogIn from "./pages/LogIn/LogIn";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import Nutrition from "./pages/Nutrition/Nutrition";
 import CustomToastWaterReminder from "./components/CustumeToast/CustumeToastWaterReminder";
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Stats from "./pages/Stats/Stats";
 import UserSearch from "./components/UserSearch/UserSearch";
 import FriendsPage from "./pages/FriendsPage/FriendsPage";
@@ -29,12 +29,14 @@ import User from "./pages/User/User";
 import AdminPage from "./pages/AdminPage/AdminPage";
 import UserMeals from "./pages/AdminPage/UserMeals";
 import Footer from "./components/Footer/Footer";
-import './App.css' 
+import "./App.css";
+import About from "./pages/About/About";
+import ErrorPage from "./pages/ErrorPage/ErrorPage";
 
 function App() {
   const [user] = useAuthState(auth);
   const [appState, setAppState] = useState({ user });
-  const [friendAlerts, setFriendAlerts] = useState(false)
+  const [friendAlerts, setFriendAlerts] = useState(false);
 
   if (appState.user !== user) {
     setAppState({ user });
@@ -42,28 +44,29 @@ function App() {
 
   useEffect(() => {
     if (user === null) return;
-  
+
     getUserData(user.uid)
       .then((snapshot) => {
         if (!snapshot.exists()) {
           throw new Error("Something went wrong!");
         }
-  
+
         const userData = snapshot.val();
         const firstKey = Object.keys(userData)[0];
-        const { role, handle, photoURL, email, phone, age, gender, weight } = userData[firstKey];
-        
+        const { role, handle, photoURL, email, phone, age, gender, weight } =
+          userData[firstKey];
+
         setAppState({
           ...appState,
           role: role,
           handle: handle,
           photo: photoURL,
           email: email,
-          phone:phone,
+          phone: phone,
           age: age,
           gender: gender,
           weight: weight,
-          friendAlerts:false
+          friendAlerts: false,
         });
       })
       .catch((error) => {
@@ -82,84 +85,91 @@ function App() {
   //   };
   // }, []);
 
-
   return (
     <ChakraProvider>
-      <AuthContext.Provider value={{...appState, appState, setAppState, friendAlerts, setFriendAlerts}}>
-      <div className="App">
-      {user && <Navbar menu ={MENU_ITEMS}/>}
-        <Routes>
-          {!user && <Route path="/" element={<Landing />}/>}
-          {!user && <Route path="/login" element={<LogIn/>}/>}
-          {!user && <Route path="/signup" element={<SignUp />}/>}
-          {user && <Route path="/" element={<DashBoard />}/>}
-          <Route 
-            path="dashboard" 
-            element={
-              <AuthenticateRoute>
-                <DashBoard />
-              </AuthenticateRoute>
-            }                      
-           />
-          <Route 
-            path="goals" 
-            element={
-              <AuthenticateRoute>
-                <Goals />
-              </AuthenticateRoute>
-            }                      
-           />
-           <Route 
-            path="stats" 
-            element={
-              <AuthenticateRoute>
-                <Stats />
-              </AuthenticateRoute>
-            }  
-           />     
-          <Route 
-            path="profile" 
-            element={
-              <AuthenticateRoute>
-                <Profile />
-              </AuthenticateRoute>
-            }  
-           />     
-           <Route 
-            path="meals" 
-            element={
-              <AuthenticateRoute>
-                <Nutrition/>
-              </AuthenticateRoute>
-            }  
-           />  
+      <AuthContext.Provider
+        value={{
+          ...appState,
+          appState,
+          setAppState,
+          friendAlerts,
+          setFriendAlerts,
+        }}
+      >
+        <div className="App">
+          {user && <Navbar menu={MENU_ITEMS} />}
+          <Routes>
+            {!user && <Route path="/" element={<Landing />} />}
+            {!user && <Route path="/login" element={<LogIn />} />}
+            {!user && <Route path="/signup" element={<SignUp />} />}
+            {user && <Route path="/" element={<DashBoard />} />}
+            <Route
+              path="dashboard"
+              element={
+                <AuthenticateRoute>
+                  <DashBoard />
+                </AuthenticateRoute>
+              }
+            />
+            <Route
+              path="goals"
+              element={
+                <AuthenticateRoute>
+                  <Goals />
+                </AuthenticateRoute>
+              }
+            />
+            <Route
+              path="stats"
+              element={
+                <AuthenticateRoute>
+                  <Stats />
+                </AuthenticateRoute>
+              }
+            />
+            <Route
+              path="profile"
+              element={
+                <AuthenticateRoute>
+                  <Profile />
+                </AuthenticateRoute>
+              }
+            />
+            <Route
+              path="meals"
+              element={
+                <AuthenticateRoute>
+                  <Nutrition />
+                </AuthenticateRoute>
+              }
+            />
 
-            <Route 
-            path="friends" 
-            element={
-              <AuthenticateRoute>
-                <FriendsPage/>
-              </AuthenticateRoute>
-            }  
-           />  
+            <Route
+              path="friends"
+              element={
+                <AuthenticateRoute>
+                  <FriendsPage />
+                </AuthenticateRoute>
+              }
+            />
 
-            <Route 
-            path="/user/:user" 
-            element={
-              <AuthenticateRoute>
-                <User/>
-              </AuthenticateRoute>
-            }  
-           /> 
+            <Route
+              path="/user/:user"
+              element={
+                <AuthenticateRoute>
+                  <User />
+                </AuthenticateRoute>
+              }
+            />
 
-            <Route 
-            path="/admin" 
-            element={
-              <AuthenticateRoute>
-                <AdminPage/>
-              </AuthenticateRoute>
-            }  
-           />
+            <Route
+              path="/admin"
+              element={
+                <AuthenticateRoute>
+                  <AdminPage />
+                </AuthenticateRoute>
+              }
+            />
 
             <Route
               path="/admin/:handle"
@@ -170,13 +180,21 @@ function App() {
               }
             />
 
+            <Route
+              path="about"
+              element={
+                <AuthenticateRoute>
+                  <About />
+                </AuthenticateRoute>
+              }
+            />
 
-        </Routes>
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
 
-        <ToastContainer position="top-center" />
-        {user && <Footer />}
-
-      </div>
+          <ToastContainer position="top-center" />
+          {user && <Footer />}
+        </div>
       </AuthContext.Provider>
     </ChakraProvider>
   );
