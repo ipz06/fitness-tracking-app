afcb822478ef40dbb64a271539478317bc685503
Ivan Damyankin <idamyankin@gmail.com>
2023-06-14 15:08:50 +0300

    Add toFixed(2) on calories

--- fitness-tracking-app/src/components/AddActivity/AddActivity.jsx
+++ fitness-tracking-app/src/components/AddActivity/AddActivity.jsx
@@ -12,10 +12,19 @@ import {
   Input,
   Button,
 } from "@chakra-ui/react";
-import { WALK_MET, RUN_MET, BIKING_MET, GYM_MET, YOGA_MET, EXERCISE_MET, SWIMMING_MET, ROW_MET } from "../../common/constants";
+import {
+  WALK_MET,
+  RUN_MET,
+  BIKING_MET,
+  GYM_MET,
+  YOGA_MET,
+  EXERCISE_MET,
+  SWIMMING_MET,
+  ROW_MET,
+} from "../../common/constants";
 import { saveActivityToDatabase } from "../../services/activity.service";
 import { AuthContext } from "../../common/context";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import { ACTIVITY_TYPE } from "../../common/constants";
 import { ONE_HOUR_IN_MIN } from "../../common/constants";
 
@@ -24,7 +33,7 @@ const AddActivity = ({ onClose, setIsModalOpen }) => {
   const [duration, setDuration] = useState("");
   const [calories, setCalories] = useState(0);
   const [caloriesError, setCaloriesError] = useState("");
-const { user, weight } = useContext(AuthContext);
+  const { user, weight } = useContext(AuthContext);
 
   const handleActivityChange = (event) => {
     setActivity(event.target.value);
@@ -37,21 +46,21 @@ const { user, weight } = useContext(AuthContext);
   const handleCalculateClick = () => {
     let calculatedCalories = 0;
     if (activity === ACTIVITY_TYPE.BIKING) {
-      calculatedCalories = BIKING_MET * weight * (duration/ONE_HOUR_IN_MIN);
+      calculatedCalories = BIKING_MET * weight * (duration / ONE_HOUR_IN_MIN);
     } else if (activity === ACTIVITY_TYPE.RUNNING) {
-      calculatedCalories = RUN_MET * weight * (duration/ONE_HOUR_IN_MIN);
+      calculatedCalories = RUN_MET * weight * (duration / ONE_HOUR_IN_MIN);
     } else if (activity === ACTIVITY_TYPE.WALKING) {
-      calculatedCalories = WALK_MET * weight * (duration/ONE_HOUR_IN_MIN);
+      calculatedCalories = WALK_MET * weight * (duration / ONE_HOUR_IN_MIN);
     } else if (activity === ACTIVITY_TYPE.GYM) {
-      calculatedCalories = GYM_MET * weight * (duration/ONE_HOUR_IN_MIN);
-    }  else if (activity === ACTIVITY_TYPE.EXERCISE) {
-      calculatedCalories = EXERCISE_MET * weight * (duration/ONE_HOUR_IN_MIN);
-    }  else if (activity === ACTIVITY_TYPE.YOGA) {
-      calculatedCalories = YOGA_MET * weight * (duration/ONE_HOUR_IN_MIN);
+      calculatedCalories = GYM_MET * weight * (duration / ONE_HOUR_IN_MIN);
+    } else if (activity === ACTIVITY_TYPE.EXERCISE) {
+      calculatedCalories = EXERCISE_MET * weight * (duration / ONE_HOUR_IN_MIN);
+    } else if (activity === ACTIVITY_TYPE.YOGA) {
+      calculatedCalories = YOGA_MET * weight * (duration / ONE_HOUR_IN_MIN);
     } else if (activity === ACTIVITY_TYPE.SWIMMING) {
-      calculatedCalories = SWIMMING_MET * weight * (duration/ONE_HOUR_IN_MIN);
+      calculatedCalories = SWIMMING_MET * weight * (duration / ONE_HOUR_IN_MIN);
     } else if (activity === ACTIVITY_TYPE.ROW) {
-      calculatedCalories = ROW_MET * weight * (duration/ONE_HOUR_IN_MIN);
+      calculatedCalories = ROW_MET * weight * (duration / ONE_HOUR_IN_MIN);
     }
     setCalories(calculatedCalories);
   };
@@ -59,29 +68,29 @@ const { user, weight } = useContext(AuthContext);
   const handleAddActivity = async () => {
     let hasError = false;
     if (calories <= 0) {
-      setCaloriesError('Please enter valid calories');
+      setCaloriesError("Please enter valid calories");
       hasError = true;
     } else {
-      setCaloriesError('');
+      setCaloriesError("");
     }
     if (hasError) {
       return;
     }
 
-	try {
+    try {
       await saveActivityToDatabase(
         user.displayName,
         user.uid,
         activity,
         duration,
-        calories,
-      )
+        calories
+      );
       onClose();
       setIsModalOpen(false);
     } catch (e) {
       console.log(e.message);
     }
-  }
+  };
 
   return (
     <Flex minHeight="100vh" width="full" align="center" justifyContent="center">
@@ -90,11 +99,7 @@ const { user, weight } = useContext(AuthContext);
           <Heading fontSize={"5xl"} paddingLeft={2}>
             Choose your activity!
           </Heading>
-          <Text
-            paddingLeft={110}
-            fontSize={"sm"}
-            color={"blue"}
-          >
+          <Text paddingLeft={110} fontSize={"sm"} color={"blue"}>
             Here you can calculate the burned calories
           </Text>
 
@@ -144,8 +149,8 @@ const { user, weight } = useContext(AuthContext);
 
           <Box>
             <Button
-			variant="outline"
-			borderColor="blue"
+              variant="outline"
+              borderColor="blue"
               bg={"white"}
               color={"blue"}
               _hover={{
@@ -156,30 +161,30 @@ const { user, weight } = useContext(AuthContext);
               minW="150px"
               onClick={handleCalculateClick}
             >
-              CALCULATE 
+              CALCULATE
             </Button>
           </Box>
-            <FormControl id="calories" isInvalid={!!caloriesError}>
-              <FormLabel>Calories Burned: Kcal</FormLabel>
-              <Input
-                size="lg"
-                placeholder="Enter calories"
-                borderRadius={3}
-                borderColor="blackAlpha.500"
-                _hover={{ borderColor: "black", borderWidth: 2 }}
-                _focus={{
-                  borderColor: "black",
-                  boxShadow: "0 0 0 3px rgba(0,0,0,0.1)",
-                }}
-                type="number"
-                value={calories}
-                onChange={(event) => setCalories(event.target.value)}
-              />
-              <FormErrorMessage>{caloriesError}</FormErrorMessage>
-            </FormControl>
-			<Box>
+          <FormControl id="calories" isInvalid={!!caloriesError}>
+            <FormLabel>Calories Burned: Kcal</FormLabel>
+            <Input
+              size="lg"
+              placeholder="Enter calories"
+              borderRadius={3}
+              borderColor="blackAlpha.500"
+              _hover={{ borderColor: "black", borderWidth: 2 }}
+              _focus={{
+                borderColor: "black",
+                boxShadow: "0 0 0 3px rgba(0,0,0,0.1)",
+              }}
+              type="number"
+              value={calories.toFixed(2)}
+              onChange={(event) => setCalories(event.target.value)}
+            />
+            <FormErrorMessage>{caloriesError}</FormErrorMessage>
+          </FormControl>
+          <Box>
             <Button
-			variant="outline"
+              variant="outline"
               bg={"blue.500"}
               color={"white"}
               _hover={{
@@ -193,7 +198,6 @@ const { user, weight } = useContext(AuthContext);
               ADD ACTIVITY
             </Button>
           </Box>
-       
         </Stack>
       </Box>
     </Flex>
@@ -202,9 +206,7 @@ const { user, weight } = useContext(AuthContext);
 
 export default AddActivity;
 
-
 AddActivity.propTypes = {
   onClose: PropTypes.func.isRequired,
   setIsModalOpen: PropTypes.func.isRequired,
-
 };
